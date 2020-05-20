a40cf9b28bb4c9f0623445ed444fc357f1c81d73
Gio Lodi <giovanni.lodi42@gmail.com>
2020-05-20 16:03:38 +1000

    Tweak label.ts so that it can be imported

--- dangerfiles/label.ts
+++ dangerfiles/label.ts
@@ -1,6 +1,9 @@
-import {warn, danger} from "danger";
+// The import is not required for the file to run standalone and it actually
+// results in a runtime error when importing this file.
+//
+// import {warn, danger} from "danger";
 
-export default async () => {
+export default async function checkLabel() {
   const labels = danger.github.issue.labels;
 
   // Warn if the PR doesn't have any labels
